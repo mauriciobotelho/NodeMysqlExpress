3cbca4e9f35aa19a8ca23dc77c0a3f737e5bb381
Mauricio Botelho <mauricio.botelho@gmail.com>
2022-07-23 18:00:54 -0300

    Criado padrao de LocalHost3000

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.get('/', (req, res) => res.json({message: 'Ok!'}));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
